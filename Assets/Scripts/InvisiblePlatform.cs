6e1d8b8497dda15177e6cbaec7c12ebb6a41d621
il-kow@mail.ru <il-kow@mail.ru>
2020-06-17 16:37:18 +0400

    init

---
+++ Assets/Scripts/InvisiblePlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InvisiblePlatform : MonoBehaviour
+{
+    private float _xPosition;
+    private GameObject _player;
+    private float _playersPosition;
+
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        _playersPosition = _player.transform.position.x;      
+        _xPosition = this.gameObject.transform.position.x;
+
+        
+        Debug.Log("Platform Position:" + _xPosition);
+        Debug.Log("PLayers Position:" + _playersPosition);
+
+        if (_playersPosition > _xPosition)
+        {
+            Debug.Log("Set visibility to true");
+
+            // Make sprite visible    
+            _xPosition = _playersPosition;
+            // this.gameObject.SetActive(true);
+        }
+    }
+}
