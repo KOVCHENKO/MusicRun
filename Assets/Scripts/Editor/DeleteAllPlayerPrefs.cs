0a1c86a04f52d80edaf3ad4030c5fa5a65d7955a
il-kow@mail.ru <il-kow@mail.ru>
2020-06-20 11:17:14 +0400

    [ver/initial]:(GameManager additional buttons)

---
+++ Assets/Scripts/Editor/DeleteAllPlayerPrefs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class DeleteAllPlayerPrefs : ScriptableObject
+    {
+        [MenuItem("Editor/Delete Player Prefs")]
+        static void DeletePrefs()
+        {
+            if (EditorUtility.DisplayDialog("Delete all player preferences?", 
+                "Are you sure to delete all the player preferences", 
+                "Yes", "No"))
+            {
+                PlayerPrefs.DeleteAll();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/GameManagerEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(GameManager))]
+    public class GameManagerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            GameManager gameManager = (GameManager) target;
+
+            if (GUILayout.Button("Reset Player State"))
+            {
+                PlayerPrefManager.ResetPlayerState(gameManager.startLives, false);
+            }
+            
+            if (GUILayout.Button("Reset High Score"))
+            {
+                PlayerPrefManager.SetHighscore(0);
+            }
+            
+            if (GUILayout.Button("Output Player State"))
+            {
+                PlayerPrefManager.ShowPlayerPrefs();
+            }
+            
+        }
+    }
+}
